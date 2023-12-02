d51a9305bdc360ea60b01b538997b4871ed9eace
Daniel Rocha <ideveloper.fstack@gmail.com>
2023-12-01 23:03:09 -0300

    Page Admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hcode;
+use \Hcode\Page;
+use Rain\Tpl;
+
+class PageAdmin extends Page
+{
+
+    public function __construct($opts = array(),$tpl_dir="/ecommerce/views/admin/")
+    {
+
+        parent::__construct($opts,$tpl_dir);
+
+    }
+}
